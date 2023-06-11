cbe69388259357eb026de443ad2ba8d65de8f55d
abhijeet <abhijeetnishal@gmail.com>
2023-06-11 09:53:35 +0530

    PostgreSQL connnection commit

--- server/src/index.ts
+++ server/src/index.ts
@@ -21,20 +21,17 @@ const port = process.env.port || 8080;
 //This will allow the user in the frontend to consume the APIs that you have created without any problem.
 app.use(cors({credentials:true, origin: ['http://localhost:3000','https://mypasswordmanager.vercel.app']}));
 
-//import database connection file
-import dbConnect from '../src/models/dbConnect';
-//execute database connection 
-dbConnect(); 
+import client from './models/dbConnect';
 
 //user Router  
-import userRouter from './routes/authRoutes';
-app.use('/auth/',userRouter)
+// import userRouter from './routes/authRoutes';
+// app.use('/auth/',userRouter)
 
-import passwordRouter from './routes/passwordRoutes';
-app.use('/passwords', passwordRouter);
+// import passwordRouter from './routes/passwordRoutes';
+// app.use('/passwords', passwordRouter);
 
 //get request when server is live
-app.get('/',(req, res)=>{
+app.get('/',(req: Request, res: Response)=>{
     res.status(200).json('Server is Live');
 })
 
--- server/src/models/dbConnect.ts
+++ server/src/models/dbConnect.ts
@@ -1,26 +1,14 @@
-//Mongoose is an object data modeling (ODM) library for MongoDB and Node.js.
-import mongoose from 'mongoose';
- 
-//configure dotenv to access secret keys
-import dotenv from 'dotenv';
-dotenv.config();
+import { Client } from "pg";
 
-//Create and export a function to house the connection:
-const dbConnect = async ()=>{
-    //import url from .env file
-    const url = process.env.DB_URL;
+//Create a new PostgreSQL client instance using the connection configuration:
+const client = new Client(`postgres://${process.env.user}:${process.env.password}@${process.env.host}/${process.env.database}`);
 
-    //use mongoose to connect this app to our database on mongoDB using the DB_URL (connection string)
-    mongoose.connect(url)
-    //Use a then catch block to show if the connection was successful or not:
-    .then(() => {
-        console.log("Successfully connected to MongoDB Database");
-    })
-    .catch((error: Error) => {
-        console.log("Unable to connect to MongoDB Database");
-        console.error(error);
-    });
-} 
+//Connect to the PostgreSQL server
+client.connect((err) => {
+    if(err)
+      console.error('Error connecting to PostgreSQL: ', err.stack);
+    else 
+      console.log('Connected to PostgreSQL database');
+});
 
-//export the dbConnect method to use in main index.ts file
-export default dbConnect;
+export default client;
