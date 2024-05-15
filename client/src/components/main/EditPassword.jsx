a13a9eee4f91636f59f7187fa870b3e52ea31a3f
Saksham1411 <sakshamsamgarg1411@gmail.com>
2024-05-15 12:56:44 +0530

    Added show and hide password On edit functionality

--- client/src/components/main/EditPassword.jsx
+++ client/src/components/main/EditPassword.jsx
@@ -1,63 +1,111 @@
-import React, { useEffect, useState } from 'react'
-import '../../styles/EditPassword.css'
-import SaveBtn from '../../assets/save-btn.png'
+import React, { useEffect, useState } from "react";
+import "../../styles/EditPassword.css";
+import eye from "../../assets/eye-image.png";
+import cutEye from "../../assets/cut-eye-image.png";
+import SaveBtn from "../../assets/save-btn.png";
 
 const EditPassword = (props) => {
-  const {item, onClose, editData, updateBtn} = props;
+  const { item, onClose, editData, updateBtn } = props;
 
-  const [websiteName, setWebsiteName] = useState('');
-  const [password, setPassword] = useState('');
-  const [message, setMessage] = useState('');
+  const [websiteName, setWebsiteName] = useState(item.websitename);
+  const [password, setPassword] = useState(item.password);
+  const [message, setMessage] = useState("");
+  const [isEyeBtnClicked, setIsEyeBtnClicked] = useState(true);
+  const [passwordType,setPasswordType] = useState('password');
 
-  //only on first render
-  useEffect(()=>{
-    setWebsiteName(item);
-  },[])
-  
-  function updateFunc(){
-    if(websiteName && password){
+  async function decrypt(passwordId){
+    setIsEyeBtnClicked(!isEyeBtnClicked);
+    if(isEyeBtnClicked){
+    const response = await fetch(`${process.env.REACT_APP_HOST_URL}/passwords/specific/${passwordId}`,{
+        method: 'GET',
+        headers: {
+            'Content-Type': 'application/json',
+        },
+        credentials: 'include',
+    });
+    
+    const data = await response.json();
+    console.log(data);
+    setPasswordType('text');
+    setPassword(data.decryptedPassword);
+  }else{
+    setPasswordType('password');
+  } 
+
+}
+
+  function updateFunc() {
+    if (websiteName && password) {
       updateBtn(true);
-      editData({websiteName, password});
-    }
-    else{
-      setMessage('Enter All Details');
+      editData({ websiteName, password });
+    } else {
+      setMessage("Enter All Details");
     }
   }
   return (
-    <div onClick={onClose} className='editOverlay'>
-      <div onClick={(e) => {e.stopPropagation();}} className='editModalContainer'>
-          <div className='editDataText'>
-            {`Edit ${item}`}
+    <div onClick={onClose} className="editOverlay">
+      <div
+        onClick={(e) => {
+          e.stopPropagation();
+        }}
+        className="editModalContainer"
+      >
+        <div className="editDataText">{`Edit ${item.websitename}`}</div>
+        <div className="websiteNameContainer">
+          <div>
+            <label htmlFor="" className="websiteName">
+              Website Name
+            </label>
           </div>
-          <div className='websiteNameContainer'>
-            <div>
-              <label htmlFor="" className='websiteName'>Website Name</label>
-            </div>
-            <div>
-                <input className='inputField' type="text" value={websiteName} onChange={(e)=>setWebsiteName(e.target.value)} placeholder='Google' />
-            </div>  
+          <div>
+            <input
+              className="inputField"
+              type="text"
+              value={websiteName}
+              onChange={(e) => setWebsiteName(e.target.value)}
+              placeholder="Google"
+            />
           </div>
-          <div className='passwordContainer'>
-            <div>
-              <label htmlFor="" className='websiteName'>Password</label>
-            </div>
-            <div>
-              <input className='inputField' type="password" value={password} onChange={(e)=>setPassword(e.target.value)} placeholder='1234567sdfgh' />
-            </div> 
+        </div>
+        <div className="passwordContainer">
+          <div>
+            <label htmlFor="" className="websiteName">
+              Password
+            </label>
           </div>
-          <div className='editBtnContainer'>
-            <button className='cancelBtn' onClick={onClose}>cancel </button>
-            <button className='saveBtn' onClick={updateFunc}>
-              <img className='saveBtnImg' src={SaveBtn} alt="" />
-              <div className='saveText'>Save</div>
+          <div className="passwordInput">
+            <input
+              className="inputField"
+              type={passwordType}
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
+              placeholder="1234567sdfgh"
+            />
+            <button
+              onClick={() => decrypt(item._id)}
+              className="eyeButton"
+            >
+              {isEyeBtnClicked ? (
+                <img src={eye} alt=""/>
+              ) : (
+                <img src={cutEye} alt=""/>
+              )}
             </button>
           </div>
-          <div className='editMessage'>
-            {message}
-          </div>
+        </div>
+        <div className="editBtnContainer">
+          <button className="cancelBtn" onClick={onClose}>
+            cancel{" "}
+          </button>
+          <button className="saveBtn" onClick={updateFunc}>
+            <img className="saveBtnImg" src={SaveBtn} alt="" />
+            <div className="saveText">Save</div>
+          </button>
+        </div>
+        <div className="editMessage">{message}</div>
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default EditPassword
+export default EditPassword;
--- client/src/components/main/PasswordPage.jsx
+++ client/src/components/main/PasswordPage.jsx
@@ -203,7 +203,7 @@ const PasswordPage = () => {
                                     {
                                         (showPopUpEdit && editId===mainData._id) && (
                                             <EditPassword
-                                                item = {mainData.websitename}
+                                                item = {mainData}
                                                 onClose={handleCloseDialogEdit}
                                                 editData = {setUpdateData}
                                                 updateBtn = {setUpdateBtnClick}
