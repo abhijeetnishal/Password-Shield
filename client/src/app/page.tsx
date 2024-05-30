2ed354b512c1712d87d0e65faa35ed01573d95fc
Vimal M <109758195+Vimall03@users.noreply.github.com>
2024-05-31 00:57:16 +0530

    Fixed Layout Isseues

--- client/src/app/page.tsx
+++ client/src/app/page.tsx
@@ -36,7 +36,7 @@ export default function LandingPages() {
   // const toggleTheme = useThemeStore((state) => state.toggleTheme);
   return (
     <>
-      
+
       <Navbar landingPage={true} />
       <div data-testid="landing-page" className=' h mx-auto'>
         <section>
@@ -68,7 +68,7 @@ export default function LandingPages() {
 
         <section id='about'>
           <div className=' '></div>
-          <div className=' md:flex  md:px-24 py-16 justify-between'>
+          <div className=' md:flex px-5 xl:px-24 py-16 justify-between'>
             <div className='p-3 md:w-96 '>
               {/* <Image src={icon1} alt='' /> */}
               <div>
@@ -99,7 +99,7 @@ export default function LandingPages() {
         </section>
 
         <section>
-          <div className={`flex py-4 items-center  xl:px-24 section3  ${theme === 'light' ? 'bg-[#f7f6ee]  text-black' : 'bg-[#12141d] text-white'}`}>
+          <div className={`flex py-4 items-center  xl:px-24  section3  ${theme === 'light' ? 'bg-[#f7f6ee]  text-black' : 'bg-[#12141d] text-white'}`}>
 
             <div className='md:px-5 px-11 py-4 md:py-36 flex-col flex align-middle'>
               <div className='xl:mr-56  '>
@@ -121,26 +121,26 @@ export default function LandingPages() {
         <section>
           <div className='flex py-4  md:px-24 items-center bg-[#fafafa]'>
 
-            <div  className='hidden md:block w-[37rem]  px-5'>
+            <div className='hidden md:block w-[37rem]  px-5'>
 
-            <Image src={illustration3}  alt="" />
+              <Image src={illustration3} alt="" />
             </div>
-            
-            <div className='px-11 md:py-36 flex-col flex align-middle'>
+
+            <div className='px-11 xl:py-24 py-11 flex-col flex align-middle'>
               <div className='xl:ml-52 '>
                 <h2 className={`md:text-5xl text-2xl ${spaceGrotesk.className} font-bold`}>All password within a reach</h2>
               </div>
               <div className='xl:ml-52' >
-                <h2 className=' md:mr-20 text-left pt-7 text-[#595a61] text-base'>Save all your password at one place within minutes. And you don't have to crack your head to remember them.</h2>
+                <h2 className=' xl:mr-20 text-left pt-7 text-[#595a61] text-base'>Save all your password at one place within minutes. And you don't have to crack your head to remember them.</h2>
               </div>
             </div>
           </div>
         </section>
 
         <section id='contact'>
-          <div className='text-center p-16 py-20 section4 bg-[#18181b]'>
+          <div className='text-center p-10 py-20 section4 bg-[#18181b]'>
             <h2 className={`text-5xl ${spaceGrotesk.className} font-bold text-white py-5`}>Join Our Community</h2>
-            <p className='text-white text-center py-5 px-20 xl:px-80'>Key Safe thrives on community support and contributions. Whether you're a developer, a security enthusiast, or simply someone who values secure password management, we welcome you to join us. Contribute to the project, report issues, or share your feedback.</p>
+            <p className='text-white text-pretty xl:text-center py-5 xl:px-80  px-0'>Key Safe thrives on community support and contributions. Whether you're a developer, a security enthusiast, or simply someone who values secure password management, we welcome you to join us. Contribute to the project, report issues, or share your feedback.</p>
             <div className='flex mx-auto justify-center font-semibold  text-white'>
               <Github />
               <Link href='https://github.com/abhijeetnishal/Password-Shield'>
@@ -157,26 +157,29 @@ export default function LandingPages() {
         </section>
 
         <section>
-          <div className='flex md:py-20 items-center px- xl:px-24  bg-[#fafafa]'>
+          <div className='xl:flex py-5 md:py-10 items-center md:px-20 xl:px-24  bg-[#fafafa]'>
 
 
-            <div className='px-5 flex-col py-10 justify-center flex align-middle'>
+            <div className='px-5 flex-col py-5 justify-center flex align-middle'>
               <div className=''>
-                <h2 className={`text-3xl  ${spaceGrotesk.className} font-bold`}>Ready to get started?</h2>
+                <h2 className={`text-3xl  text-balance xl:text-left  ${spaceGrotesk.className} font-bold`}>Ready to get started?</h2>
               </div>
               <div>
-                <h2 className='text-left xl:mr-40 pt-5 pb-3 text-[#595a61] text-base'>Dive into a world where password management is no longer a headache. Our platform welcomes you to streamline your digital security effortlessly.</h2>
-                <Link href='/auth/login'>
-                  <button className='bg-[#ef4444] shadow-md hover:bg-[#e04747] text-white py-3 px-8'>
-                    Login
-                  </button>
-                </Link>
+                <h2 className=' text-balance xl:text-left xl:mr-40 pt-4  pb-3 text-[#595a61] text-base'>Dive into a world where password management is no longer a headache. Our platform welcomes you to streamline your digital security effortlessly.</h2>
+                <div className='text-'>
+
+                  <Link href='/auth/login'>
+                    <button className='bg-[#ef4444] shadow-md  hover:bg-[#e04747] text-white py-3 px-5'>
+                      Login
+                    </button>
+                  </Link>
+                </div>
               </div>
 
             </div>
 
-            <div className='md:flex py-10  justify-center'>
-              <div className='xl:mx-10 px-5 my-2 w-72 border-2 rounded py-5 bg-white'>
+            <div className='md:flex py-10  align-middle justify-center'>
+              <div className='xl:mx-10 mx-auto px-5 my-2 w-72 border-2 rounded py-5 bg-white'>
                 {/* <Image src={icon5} alt='' /> */}
                 <div>
                   <Icon5></Icon5>
@@ -184,7 +187,7 @@ export default function LandingPages() {
                 <h2 className='font-bold py-3'>Store data on cloud</h2>
                 <p className='text-[#595a61]'>Safeguard your sensitive information with confidence on our platform, where data protection is our top priority.  </p>
               </div>
-              <div className='xl:mx-10 px-5 w-72 my-2 border-2 rounded py-5 bg-white'>
+              <div className='xl:mx-10 mx-auto px-5 w-72 my-2 border-2 rounded py-5 bg-white'>
                 {/* <Image src={icon4} alt='' /> */}
                 <div>
                   <Icon4></Icon4>
