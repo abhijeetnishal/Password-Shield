24963b32e9431de7eb774a114f2fe4f9f0a547f9
abhijeetnishal <abhijeetnishal@gmail.com>
2025-06-15 17:21:50 +0530

    refactor: Password server

--- server/dist/controllers/password.js
+++ server/dist/controllers/password.js
@@ -14,17 +14,58 @@ const dbConnect_1 = require("../config/dbConnect");
 const user_1 = require("../services/user");
 const encryption_1 = require("../utils/encryption");
 const passwords_1 = require("../services/passwords");
+const createPassword = (req, res) => __awaiter(void 0, void 0, void 0, function* () {
+    try {
+        const id = req._id;
+        const user = yield (0, user_1.getDetails)("_id", id);
+        if (user) {
+            const { title, website, username, password, category, notes, favorite } = req.body;
+            if (!title || !username || !password || !category) {
+                return res.status(400).json({ message: "Required fields missing" });
+            }
+            else {
+                //encrypt the password before storing to db
+                const data = (0, encryption_1.encrypt)(password);
+                const encryptedPassword = data.encryptedData;
+                const base64data = data.base64data;
+                yield dbConnect_1.pool.query(`INSERT INTO passwords(title, website, username, password, category, notes, favorite, iv, user_id)
+            VALUES($1, $2, $3, $4, $5, $6, $7, $8, $9)`, [
+                    title,
+                    website,
+                    username,
+                    encryptedPassword,
+                    category,
+                    notes,
+                    favorite,
+                    base64data,
+                    id,
+                ]);
+                return res
+                    .status(201)
+                    .json({ data: null, message: "Password data saved" });
+            }
+        }
+        else {
+            return res.status(404).json({ message: "User doesn't exist" });
+        }
+    }
+    catch (error) {
+        console.log(error);
+        return res.status(500).json({ message: "Internal Server Error" });
+    }
+});
+exports.createPassword = createPassword;
 const getAllPasswords = (req, res) => __awaiter(void 0, void 0, void 0, function* () {
-    const { search, limit, offset } = req.query;
+    const { query, limit, offset } = req.query;
     const id = req._id;
     try {
         const user = yield (0, user_1.getDetails)("_id", id);
         if (user) {
             const queryParams = [id];
             let queryStr = "SELECT * FROM passwords WHERE user_id = $1";
-            if (search) {
-                queryParams.push(`%${search}%`);
-                queryStr += " AND website_name ILIKE $" + queryParams.length;
+            if (query) {
+                queryParams.push(`%${query}%`);
+                queryStr += " AND title ILIKE $" + queryParams.length;
             }
             queryStr += " ORDER BY created_at";
             if (limit) {
@@ -52,38 +93,6 @@ const getAllPasswords = (req, res) => __awaiter(void 0, void 0, void 0, function
     }
 });
 exports.getAllPasswords = getAllPasswords;
-const createPassword = (req, res) => __awaiter(void 0, void 0, void 0, function* () {
-    try {
-        const id = req._id;
-        const user = yield (0, user_1.getDetails)("_id", id);
-        if (user) {
-            const { title, description, websiteName, password } = req.body;
-            if (!websiteName || !password) {
-                return res.status(400).json({ message: "Required fields missing" });
-            }
-            else {
-                //encrypt the password before storing to db
-                const data = (0, encryption_1.encrypt)(password);
-                const encryptedPassword = data.encryptedData;
-                const base64data = data.base64data;
-                const { rows } = yield dbConnect_1.pool.query(`INSERT INTO passwords(title, description, website_name, password, iv, user_id)
-            VALUES($1, $2, $3, $4, $5, $6)
-            RETURNING *`, [title, description, websiteName, encryptedPassword, base64data, id]);
-                return res
-                    .status(201)
-                    .json({ data: rows[0], message: "Password data saved" });
-            }
-        }
-        else {
-            return res.status(404).json({ message: "User doesn't exist" });
-        }
-    }
-    catch (error) {
-        console.log(error);
-        return res.status(500).json({ message: "Internal Server Error" });
-    }
-});
-exports.createPassword = createPassword;
 const updatePassword = (req, res) => __awaiter(void 0, void 0, void 0, function* () {
     try {
         const id = req._id;
@@ -92,7 +101,7 @@ const updatePassword = (req, res) => __awaiter(void 0, void 0, void 0, function*
             const { id } = req.params;
             const passwordExists = yield (0, passwords_1.getPasswordDetails)("_id", id);
             if (passwordExists) {
-                const { title, description, websiteName, password } = req.body;
+                const { title, website, username, password, category, notes, favorite, } = req.body;
                 // Build the update query dynamically
                 const updateFields = [];
                 const updateValues = [];
@@ -100,13 +109,13 @@ const updatePassword = (req, res) => __awaiter(void 0, void 0, void 0, function*
                     updateFields.push(`title = $${updateValues.length + 1}`);
                     updateValues.push(title);
                 }
-                if (description && description !== "") {
-                    updateFields.push(`description = $${updateValues.length + 1}`);
-                    updateValues.push(description);
+                if (website && website !== "") {
+                    updateFields.push(`website = $${updateValues.length + 1}`);
+                    updateValues.push(website);
                 }
-                if (websiteName && websiteName !== "") {
-                    updateFields.push(`website_name = $${updateValues.length + 1}`);
-                    updateValues.push(websiteName);
+                if (username && username !== "") {
+                    updateFields.push(`username = $${updateValues.length + 1}`);
+                    updateValues.push(username);
                 }
                 if (password) {
                     //encrypt the password before storing to db
@@ -118,15 +127,30 @@ const updatePassword = (req, res) => __awaiter(void 0, void 0, void 0, function*
                     updateFields.push(`iv = $${updateValues.length + 1}`);
                     updateValues.push(base64data);
                 }
+                if (category && category !== "") {
+                    updateFields.push(`category = $${updateValues.length + 1}`);
+                    updateValues.push(category);
+                }
+                if (notes && notes !== "") {
+                    updateFields.push(`notes = $${updateValues.length + 1}`);
+                    updateValues.push(notes);
+                }
+                if (favorite !== null) {
+                    updateFields.push(`favorite = $${updateValues.length + 1}`);
+                    updateValues.push(favorite);
+                }
                 if (updateFields.length > 0) {
                     updateFields.push(`updated_at = $${updateValues.length + 1}`);
                     updateValues.push(new Date().toISOString());
-                    const updateQuery = `UPDATE passwords SET ${updateFields.join(", ")} WHERE _id = $${updateValues.length + 1} RETURNING *`;
+                    const updateQuery = `UPDATE passwords SET ${updateFields.join(", ")} WHERE _id = $${updateValues.length + 1}`;
                     updateValues.push(id);
-                    const { rows } = yield dbConnect_1.pool.query(updateQuery, updateValues);
+                    yield dbConnect_1.pool.query(updateQuery, updateValues);
                     return res
                         .status(200)
-                        .json({ data: rows[0], message: "Data updated successfully" });
+                        .json({
+                        data: null,
+                        message: "Password data updated successfully",
+                    });
                 }
                 else {
                     return res.status(200).json({ message: "No fields to update" });
